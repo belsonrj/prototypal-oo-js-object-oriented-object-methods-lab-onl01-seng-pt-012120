ca5b88bc5c852cc2cc35de2fb84114c46e3a5575
Rick Belson <belsonrj@aol.com>
2020-08-20 01:30:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function BoardMember(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-  }
+}
   
-  BoardMember.prototype.veto = function() {
-    console.log(`No, I must disagree`);
-  };
+BoardMember.prototype.veto = function() {
+  console.log(`No, I must disagree`);
+};
   
  
